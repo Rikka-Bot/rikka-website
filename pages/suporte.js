83b51d97a227548d599ce86ea12a5a1e726fc806
Leonardo <70207858+Leonardo-ST@users.noreply.github.com>
2022-11-02 15:49:04 -0300

    div na página de suporte(tô fazendo)

--- pages/suporte.js
+++ pages/suporte.js
@@ -9,8 +9,12 @@ function suporte() {
     <Head>
         <title>Suporte</title>
     </Head>
+    <div>
     <h1 className={styles.support}>Precisando de ajuda?</h1>
     <h3 className={styles.support}>Entre no servidor Rikka Land e fale diretamente com os desenvolvedores:</h3>
+    </div>
+
+
     </>
     )
 }
