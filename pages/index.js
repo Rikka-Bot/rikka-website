e20ba517a3e99fa9bae659fef5f690c942af3bab
Leonardo <70207858+Leonardo-ST@users.noreply.github.com>
2024-03-13 10:05:34 -0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -19,13 +19,13 @@ function Home() {
       <meta name="theme-color" content="#00d1ff" />
 </Head>
     <div className={styles.container}>   
-      <h1 className={styles.titulo}>Ohayo Amigo(a)!<br />Seja bem-vindo(a) ao meu website!</h1>
+      <h1 className={styles.titulo}><strong>Projeto da Rikka foi encerrado, obrigado aa todos que usaram ela!</strong></h1>
       <br /><br />
       <Image className={styles.imagem} src='/Rikka-no-back.png' width='300px' height='250px' alt='Rikka' />
-      <h3 className={styles.meio}>Me convide para seu servidor:<br /><a href='https://discord.com/api/oauth2/authorize?client_id=770762400034848808&permissions=8&scope=bot%20applications.commands'><Button variant='contained'><strong>Convite</strong></Button></a></h3>
+     /* <h3 className={styles.meio}>Me convide para seu servidor:<br /><a href='https://discord.com/api/oauth2/authorize?client_id=770762400034848808&permissions=8&scope=bot%20applications.commands'><Button variant='contained'><strong>Convite</strong></Button></a></h3>
       <br/>
       <h3 className={styles.meio2}>Acesse o painel:<br /><Button variant='contained' disabled><strong>Em desenvolvimento</strong></Button></h3>
-    
+    */
     </div>
     </>)
 }
