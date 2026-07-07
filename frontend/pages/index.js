2bd85f7304a48a2162badfa43f5827b70c1b0758
Leonardo-ST <tavaresleonardo567@gmail.com>
2026-07-07 12:16:55 -0300

    mudanças na home

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -18,13 +18,31 @@ function Home() {
       <meta name="theme-color" content="#00d1ff" />
 </Head>
     <div className={styles.container}>   
-      <h1 className={styles.titulo}><strong>Projeto da Rikka foi encerrado, obrigado a todos que usaram ela!</strong></h1>
+      <h1 className={styles.titulo}><strong>Rikka discord bot</strong></h1>
       <br /><br />
       <Image className={styles.imagem} src="/Rikka-no-back.png" width={300} height={250} alt="Rikka" />
      <h3 className={styles.meio}>Me convide para seu servidor:<br /><a href='https://discord.com/api/oauth2/authorize?client_id=770762400034848808&permissions=8&scope=bot%20applications.commands'><Button variant='contained'><strong>Convite</strong></Button></a></h3>
       <br/>
-      <h3 className={styles.meio2}>Acesse o painel:<br /><Button variant='contained' disabled><strong>Em desenvolvimento</strong></Button></h3>
-    
+      <h3 className={styles.meio2}>
+  ⭐ Enjoying Rikka?
+  <br />
+  Leave a vote on top.gg!
+  <br /><br />
+
+  <div className={styles.topgg}>
+  <a
+    href="https://top.gg/bot/770762400034848808"
+    target="_blank"
+    rel="noopener noreferrer"
+  >
+    {/* eslint-disable-next-line @next/next/no-img-element */}
+    <img
+      src="https://top.gg/api/widget/770762400034848808.svg"
+      alt="Vote for Rikka"
+    />
+  </a>
+</div>
+</h3>
     </div>
     </>)
 }
